2904eeff480a79fe62eefe9706b5386311bf0c6b
Nguyen Khac Trieu <trieunk@yahoo.com>
2016-11-24 17:33:47 +0700

    feat: Add generate release notes base on Jira issues

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = {
+  generateReleaseNotes: require('./lib/jira')
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var generator = require('./')
+
+generator.generateReleaseNotes({
+  // outputFile: 'ReleaseNotes.md',
+  project: 'BCC E-Learning',
+  prefix: 'BCCEL',
+  version: 'v1.4.0-build.4'
+})
