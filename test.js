7adfac89af0f9b8661e4a3db6e71b3c2a7c47399
Nguyen Khac Trieu <trieunk@yahoo.com>
2016-11-25 00:49:09 +0700

    feat: Add callback on generate done
    + Add test for prompt JIRA_USERNAME and JIRA_PASSWORD

--- test.js
+++ test.js
@@ -1,10 +1,42 @@
 'use strict'
 
 var generator = require('./')
+var prompt = require('prompt')
 
-generator.generateReleaseNotes({
-  // outputFile: 'ReleaseNotes.md',
-  project: 'BCC E-Learning',
-  prefix: 'BCCEL',
-  version: 'v1.4.0-build.4'
-})
+if (typeof process.env.JIRA_USERNAME === 'undefined') {
+  var schema = {
+    properties: {
+      name: {
+        description: 'Enter your Jira user name',
+        required: true
+      },
+      password: {
+        description: 'Enter your Jira password',
+        hidden: true
+      }
+    }
+  }
+  prompt.start()
+
+  prompt.get(schema, (err, result) => {
+    if (err) {
+      console.error(err)
+    }
+
+    process.env['JIRA_USERNAME'] = result.name
+    process.env['JIRA_PASSWORD'] = result.password
+
+    generate()
+  })
+} else {
+  generate()
+}
+
+function generate () {
+  generator.generateReleaseNotes({
+    // outputFile: 'ReleaseNotes.md',
+    project: 'BCC E-Learning',
+    prefix: 'BCCEL',
+    version: 'v1.4.0-build.4'
+  })
+}
